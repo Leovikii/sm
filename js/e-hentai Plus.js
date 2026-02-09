ba7c1b6588f1610b310d8481fcc7dd71f23d88d1
Viki <94736133+Leovikii@users.noreply.github.com>
2026-02-09 21:56:39 +0800

    Update e-hentai Plus.js

--- js/e-hentai Plus.js
+++ js/e-hentai Plus.js
@@ -2,6 +2,7 @@
 // @name         e-hentai Plus
 // @name:zh-CN   E-Hentai Plus
 // @namespace    http://tampermonkey.net/
+//@homepageURL   https://github.com/Leovikii/sm/tree/main/js
 // @version      2.1
 // @description  Continuous reading mode with floating page control and ultra-fast loading
 // @description:zh-CN E-Hentai 的增强型连续阅读模式，具有高级功能和优化。
